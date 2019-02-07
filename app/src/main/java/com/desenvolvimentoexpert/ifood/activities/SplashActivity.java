269e44c3a7ee7d8d407aa08d471acb2a3c93ab9c
jhoni-costa <jhoni_costa@outlook.com>
2019-02-07 16:37:01 -0200

    Configuracoes do firebase e Splash Activity

---
+++ app/src/main/java/com/desenvolvimentoexpert/ifood/activities/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.desenvolvimentoexpert.ifood.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.desenvolvimentoexpert.ifood.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goIntent();
+            }
+        }, 3000);
+
+    }
+
+    private void goIntent() {
+        Intent i = new Intent(SplashActivity.this, AutenticacaoActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
